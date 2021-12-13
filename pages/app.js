76d252f18d0a6694587d7b9218f65d509d3a0991
Osvaldo Arzate Santiago <oarzate@ensamble.dev>
2021-12-13 11:38:49 -0600

    🐛 resolved bug

--- pages/app.js
+++ pages/app.js
@@ -2,14 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export async function getStaticProps() {
-  return {
-    props: {
-      apiUrl: process.env.API_URL
-    }
-  };
-}
-
 export default function App(props) {
   return (
     <div className={styles.container}>
@@ -24,10 +16,7 @@ export default function App(props) {
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>{props.apiUrl}</code>
-        </p>
+        <p className={styles.description}>Get started by editing </p>
 
         <div className={styles.grid}>
           <a href='https://nextjs.org/docs' className={styles.card}>
